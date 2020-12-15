0b6437d4a5432b2ed750535c713b28befc6a64b9
Falk Zimmermann <falkzimmermann@Falks-MacBook-Air.local>
2020-12-15 21:46:45 +0100

    learning github once more

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ class Site extends React.Component {
         <h1>Willkommen auf der Website des DorfQuartier!</h1>
         <p>Da wir momentan unsere Website restrukturieren, entschuldigen wir uns,
         für eventuelle Unannehmlichkeiten diesbezüglich.
-        <br />Bei dringenden Anfragen, erreichen Sie uns nach wie vor unter:
+        <br />Bei1 Anfragen, erreichen Sie uns nach wie vor unter:
         <br /><b>+49 5971 8 99 95 31</b>
         </p>
       </div>
