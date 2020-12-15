ba6b80952d051ee1cece409950295980314354c2
Falk Zimmermann <falkzimmermann@Falks-MacBook-Air.local>
2020-12-15 01:51:12 +0100

    Trying to find the right subfolder

--- amplifyapp/src/index.js
+++ amplifyapp/src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Site extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Willkommen auf der Website des DorfQuartier!</h1>
+        <p>Da wir momentan unsere Website restrukturieren, entschuldigen wir uns,
+        für eventuelle Unannehmlichkeiten diesbezüglich.
+        <br />Bei dringenden Anfragen, erreichen Sie uns nach wie vor unter:
+        <br /><b>+49 5971 8 99 95 31</b>
+        </p>
+      </div>
+    );
+  }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Site />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
